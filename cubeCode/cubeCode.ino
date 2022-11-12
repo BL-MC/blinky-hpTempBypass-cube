6ecb55b21daf74126a3993b96a5c5d8d4216551e
David McGinnis <david.mcginnis@bl-mc.se>
2022-11-12 11:37:22 +0100

    new class structure

--- cubeCode/cubeCode.ino
+++ cubeCode/cubeCode.ino
@@ -1,78 +1,39 @@
-
-const char* ssid = "Tele2_2BA356";
-const char* wifiPassword = "";
-const char* mqttServer = "hip-lifeguard.cloudmqtt.com";
-const char* mqttUsername = "";
-const char* mqttPassword = "";
-const char* mqttPublishTopic = "blinky-hpTempBypass/01/reading";
-const char* mqttSubscribeTopic = "blinky-hpTempBypass/01/setting";
-
-
-#define BLINKYMQTTBUSBUFSIZE  3
-union BlinkyBusUnion
+#include "BlinkyMqttCube.h"
+struct
 {
-  struct
-  {
-    int16_t state;
-    int16_t watchdog;
-    int16_t relay;
-  };
-  int16_t buffer[BLINKYMQTTBUSBUFSIZE];
-} blinkyBus;
+  int16_t state;
+  int16_t watchdog;
+  int16_t relay;
+} cubeData;
+
 void subscribeCallback(uint8_t address, int16_t value)
 {
   if (address == 2) setRelay();
 }
 
-#include "blinky-wifiMqtt-cube.h"
 int relayPin = 22;
-int wirelessConnectedPin = 10;
 int relayLedPin = 12;
-unsigned long tnow;
-unsigned long lastWirelessBlink;
-boolean wirelessLed = false;
-
 
 void setup() 
 {
   pinMode(relayPin, OUTPUT);
-  pinMode(wirelessConnectedPin, OUTPUT);
   pinMode(relayLedPin, OUTPUT);
-  digitalWrite(wirelessConnectedPin, 1);
   Serial.begin(115200);
-  delay(5000);
-  initBlinkyBus(2000,true, 11);
+  delay(1000);
+  BlinkyMqttCube.setChattyCathy(false);
+  BlinkyMqttCube.init(2000,true, 10, 16, (int16_t*)& cubeData,  sizeof(cubeData), subscribeCallback);
 
-  blinkyBus.relay = 0;
+  cubeData.relay = 0;
   setRelay();
-  digitalWrite(wirelessConnectedPin, 0);
-  tnow = millis();
-  lastWirelessBlink = tnow;
 }
 
 void loop() 
 {
-  tnow = millis();
-  blinkyBusLoop();
-//  publishBlinkyBusNow(); 
-  if (g_wifiStatus == WL_CONNECTED)
-  {
-    wirelessLed = true;
-    digitalWrite(wirelessConnectedPin, wirelessLed);
-  }
-  else
-  {
-    if ((tnow - lastWirelessBlink) > 1000)
-    {
-      wirelessLed = !wirelessLed;
-      lastWirelessBlink = tnow;
-      digitalWrite(wirelessConnectedPin, wirelessLed);
-    }
-  }
-
+  BlinkyMqttCube.loop();
 }
+
 void setRelay()
 {
-  digitalWrite(relayPin, blinkyBus.relay);
-  digitalWrite(relayLedPin, blinkyBus.relay);
+  digitalWrite(relayPin, cubeData.relay);
+  digitalWrite(relayLedPin, cubeData.relay);
 }
