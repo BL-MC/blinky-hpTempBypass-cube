50ddbe041ab189a3648b114b720c3afcd0accf4e
David McGinnis <david.mcginnis@bl-mc.se>
2022-11-15 15:16:42 +0100

    added rp watchdog

--- cubeCode/cubeCode.ino
+++ cubeCode/cubeCode.ino
@@ -18,10 +18,10 @@ void setup()
 {
   pinMode(relayPin, OUTPUT);
   pinMode(relayLedPin, OUTPUT);
-  Serial.begin(115200);
+//  Serial.begin(115200);
   delay(1000);
   BlinkyMqttCube.setChattyCathy(false);
-  BlinkyMqttCube.init(2000,true, 10, 16, (int16_t*)& cubeData,  sizeof(cubeData), subscribeCallback);
+  BlinkyMqttCube.init(2000,true, 10, 100, 16, (int16_t*)& cubeData,  sizeof(cubeData), subscribeCallback);
 
   cubeData.relay = 0;
   setRelay();
@@ -35,5 +35,7 @@ void loop()
 void setRelay()
 {
   digitalWrite(relayPin, cubeData.relay);
-  digitalWrite(relayLedPin, cubeData.relay);
+  int ledIntensity = 0;
+  if (cubeData.relay > 0) ledIntensity = 50;
+  analogWrite(relayLedPin, ledIntensity);
 }
